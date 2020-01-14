ab724a95c9103a3dffb62ed373d8bf67c7a91f13
hensby <whc0721a@vip.qq.com>
2020-01-14 14:00:00 -0800

    graph

---
+++ src/FindAllNumberDisappear.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class FindAllNumberDisappear {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+
+        // Iterate over each of the elements in the original array
+        for (int i = 0; i < nums.length; i++) {
+
+            // Treat the value as the new index
+            int newIndex = Math.abs(nums[i]) - 1;
+
+            // Check the magnitude of value at this new index
+            // If the magnitude is positive, make it negative
+            // thus indicating that the number nums[i] has
+            // appeared or has been visited.
+            if (nums[newIndex] > 0) {
+                nums[newIndex] *= -1;
+            }
+        }
+
+        // Response array that would contain the missing numbers
+        List<Integer> result = new LinkedList<Integer>();
+
+        // Iterate over the numbers from 1 to N and add all those
+        // that have positive magnitude in the array
+        for (int i = 1; i <= nums.length; i++) {
+
+            if (nums[i - 1] > 0) {
+                result.add(i);
+            }
+        }
+
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        int[] a = new int[]{4,3,2,7,8,2,3,1};
+        FindAllNumberDisappear findAllNumberDisappear = new FindAllNumberDisappear();
+        System.out.println(findAllNumberDisappear.findDisappearedNumbers(a));
+    }
+}
