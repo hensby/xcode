f8b00656903ec498c6e0ff184a11892d630d6aa7
hensby <whc0721a@vip.qq.com>
2020-01-15 14:56:18 -0800

    graph

--- src/gragh/CurrencyExchange.java
+++ src/gragh/CurrencyExchange.java
@@ -5,32 +5,32 @@ import java.util.*;
 public class CurrencyExchange {
 
     /**
-     Given a file containing ratios between labels and query file containing pair of labels, find the ratio between each label pair (write it to an output file)
-
-
-     Input file example:
-
-     Source		Ratio		Destination
-     GBP		0.69		USD
-     Yard		1.09		Meter
-     EUR		0.0077		YEN
-     YEN		167.75		GBP
-
-
-     Query file example:
-
-     Source		Destination
-     EUR		USD
-     Meter		Yard
-
-     Output file example:
-
-     Source		Ratio		Destination
-     EUR		0.89		USD
-     Meter		0.91		Yard
-
-
-     Write a program that produces a list of ratio records using the provided ratio records and queries.
+     * Given a file containing ratios between labels and query file containing pair of labels, find the ratio between each label pair (write it to an output file)
+     * <p>
+     * <p>
+     * Input file example:
+     * <p>
+     * Source		Ratio		Destination
+     * GBP		0.69		USD
+     * Yard		1.09		Meter
+     * EUR		0.0077		YEN
+     * YEN		167.75		GBP
+     * <p>
+     * <p>
+     * Query file example:
+     * <p>
+     * Source		Destination
+     * EUR		USD
+     * Meter		Yard
+     * <p>
+     * Output file example:
+     * <p>
+     * Source		Ratio		Destination
+     * EUR		0.89		USD
+     * Meter		0.91		Yard
+     * <p>
+     * <p>
+     * Write a program that produces a list of ratio records using the provided ratio records and queries.
      */
     private static class Ratio {
         String num;
@@ -54,44 +54,39 @@ public class CurrencyExchange {
         }
     }
 
-    double[] queryRatios(List<Ratio> ratios, List<Pair> queries) {
+    public double[] queryRatios(List<Ratio> ratios, List<Pair> queries) {
         Map<String, Map<String, Double>> adjList = new HashMap<>();
         for (Ratio ratio : ratios) {
             adjList.computeIfAbsent(ratio.num, k -> new HashMap<>()).put(ratio.denom, ratio.ratio);
             adjList.computeIfAbsent(ratio.denom, k -> new HashMap<>()).put(ratio.num, 1.0 / ratio.ratio);
         }
-
-        double[] result = new double[queries.size()];
+        int queueLength = queries.size();
+        double[] result = new double[queueLength];
         Map<String, Double> memorization = new HashMap<>();
-        for (int i = 0; i < result.length; i++) {
+        for (int i = 0; i < queueLength; i++) {
             Pair query = queries.get(i);
             result[i] = processQuery(query.first, query.second, adjList, memorization);
         }
-
         return result;
+
+
     }
 
-    private static final double EPSILON = 0.001;
+    private static final double EPSILON = 0.00001;
 
     // Map<String, Double> memorization, Set<String> visitedSet
     // -1.0 not possible
     // -2.0 as visited
     private double processQuery(String num, String denom, Map<String, Map<String, Double>> adjList,
                                 Map<String, Double> memorization) {
+
+        System.out.println("memorization " + memorization);
         String key = num + ":" + denom;
         Double res = memorization.get(key);
-
-        if (res != null) {
-            return res;
-        }
-
-        if (!adjList.containsKey(num) || !adjList.containsKey(denom)) {
-            return -1.0;
-        }
-
-        if (num.equals(denom)) {
-            return 1.0;
-        }
+        System.out.println(key);
+        if (res != null) return res;
+        if (!adjList.containsKey(num) || !adjList.containsKey(denom)) return -1.0;
+        if (num.equals(denom)) return 1.0;
 
         Map<String, Double> numMap = adjList.get(num);
         Double d = numMap.get(denom);
@@ -126,36 +121,37 @@ public class CurrencyExchange {
 
     public static void main(String[] args) {
         CurrencyExchange solution = new CurrencyExchange();
-//		List<Ratio> ratios = new ArrayList<>() {{
-//			add(new Ratio("USD", "GBP", 0.69));
-//			add(new Ratio("YEN", "EUR", 0.0077));
-//			add(new Ratio("GBP", "YEN", 167.75));
-//			add(new Ratio("USD", "RP", 212.02));
-//			add(new Ratio("Yard", "Meter", 1.09));
-//		}};
-//		List<Pair> queries = new ArrayList<>() {{
-//			add(new Pair("USD", "EUR"));
-//			add(new Pair("YEN", "YEN"));
-//			add(new Pair("Meter", "Yard"));
-//			add(new Pair("Meter", "RP")); //should return -1.0
-//			add(new Pair("Yard", "RP"));  //should return -1.0
-//		}};
+        List<Ratio> ratios = new ArrayList<>() {{
+            add(new Ratio("USD", "GBP", 0.69));
+            add(new Ratio("YEN", "EUR", 0.0077));
+            add(new Ratio("GBP", "YEN", 167.75));
+            add(new Ratio("USD", "RP", 212.02));
+            add(new Ratio("Yard", "Meter", 1.09));
+        }};
+        List<Pair> queries = new ArrayList<>() {{
+            add(new Pair("USD", "EUR"));
+			add(new Pair("YEN", "YEN"));
+			add(new Pair("Meter", "Yard"));
+			add(new Pair("Meter", "RP")); //should return -1.0
+			add(new Pair("Yard", "RP"));  //should return -1.0
+        }};
 
-        List<Ratio> ratios = Arrays.asList(
-                new Ratio("a","b", 1.0), new Ratio("a","c", 2.0),
-                new Ratio("a","d", 3.0), new Ratio("a","e", 4.0),
-                new Ratio("a","f", 5.0), new Ratio("a","g", 6.0),
-                new Ratio("a","h", 7.0), new Ratio("a","i", 8.0),
-                new Ratio("a","j", 9.0), new Ratio("a","k", 10.0),
-                new Ratio("a","l", 11.0), new Ratio("a","aa", 1.0),
-                new Ratio("a","aaa", 1.0), new Ratio("a","aaaa", 1.0),
-                new Ratio("a","aaaa", 1.0), new Ratio("a","bb", 1.0),
-                new Ratio("a","bbb", 3.0), new Ratio("a","ff", 5.0));
-        List<Pair> queries = Arrays.asList(new Pair("d","f"), new Pair("e","g"), new Pair("e","k"),
-                new Pair("h","a"), new Pair("aaa","k"));
+//        List<Ratio> ratios = Arrays.asList(
+//                new Ratio("a", "b", 1.0), new Ratio("a", "c", 2.0),
+//                new Ratio("a", "d", 3.0), new Ratio("a", "e", 4.0),
+//                new Ratio("a", "f", 5.0), new Ratio("a", "g", 6.0),
+//                new Ratio("a", "h", 7.0), new Ratio("a", "i", 8.0),
+//                new Ratio("a", "j", 9.0), new Ratio("a", "k", 10.0),
+//                new Ratio("a", "l", 11.0), new Ratio("a", "aa", 1.0),
+//                new Ratio("a", "aaa", 1.0), new Ratio("a", "aaaa", 1.0),
+//                new Ratio("a", "aaaa", 1.0), new Ratio("a", "bb", 1.0),
+//                new Ratio("a", "bbb", 3.0), new Ratio("a", "ff", 5.0));
+//        List<Pair> queries = Arrays.asList(new Pair("d", "f"), new Pair("e", "g"), new Pair("e", "k"),
+//                new Pair("h", "a"), new Pair("aaa", "k"));
         double[] res = solution.queryRatios(ratios, queries);
-        for (double ratio: res) {
-            System.out.println(ratio);
+        for (int i = 0; i < res.length; i++) {
+            System.out.print(queries.get(i).first + " " + queries.get(i).second + " ");
+            System.out.println(res[i]);
         }
 
     }
---
+++ src/gragh/UnionFind.java
@@ -0,0 +1,36 @@
+package gragh;
+
+class UnionFind {
+    int[] id, size;
+    int count;
+
+    public UnionFind(int len) {
+        this.id = new int[len];
+        this.size = new int[len];
+        this.count = len;
+    }
+
+    public boolean find(int p, int q) {
+        return root(p) == root(q);
+    }
+
+    public void union(int p, int q) {
+        int pi = root(p), qi = root(q);
+        if (this.size[pi] < this.size[qi]) {
+            this.id[pi] = qi;
+            this.size[qi] += this.size[pi];
+        } else {
+            this.id[qi] = pi;
+            this.size[pi] += this.size[qi];
+        }
+        this.count--;
+    }
+
+    public int root(int i) {
+        while (i != id[i]) {
+            id[i] = id[id[i]]; // path compression
+            i = id[i];
+        }
+        return i;
+    }
+}
