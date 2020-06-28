83626d6b71522c805bf7827c8b4b63a360dff935
hensby <whc0721a@outlook.com>
2020-06-28 14:28:10 -0700

    6,26

--- src/SlidingWindow/FindKLengthSubstringsWithNoRepeatedCharacters.java
+++ src/SlidingWindow/FindKLengthSubstringsWithNoRepeatedCharacters.java
@@ -5,36 +5,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class FindKLengthSubstringsWithNoRepeatedCharacters {
-    public int numKLenSubstrNoRepeats(String S, int K) {
-        if(S == null || S.length() < K){
-            return 0;
-        }
-
-        int [] map = new int[26];
-        int runner = 0;
-        int count = 0;
-        int res = 0;
-        while(runner < S.length()){
-            System.out.println(Arrays.toString(map));
-            if(map[S.charAt(runner)-'a']++ > 0){
-                count++;
-            }
-
-            if(runner >= K){
-                if(map[S.charAt(runner-K)-'a']-- > 1){
-                    count--;
-                }
-            }
-
-            if(runner >=K-1 && count == 0){
-                res++;
-            }
-
-            runner++;
-        }
-
-        return res;
-    }
 
 
     public int numKLenSubstrNoRepeats1(String S, int K) {
@@ -47,10 +17,30 @@ public class FindKLengthSubstringsWithNoRepeatedCharacters {
             map.put(S.charAt(j), map.getOrDefault(S.charAt(j), 0) + 1);
         }
         for (; j < S.length(); j++,i++) {
+            System.out.println(map);
             map.put(S.charAt(j), map.getOrDefault(S.charAt(j), 0) + 1);
             if (map.size() == K) res++;
             map.put(S.charAt(i), map.getOrDefault(S.charAt(i), 0) - 1);
             if (map.get(S.charAt(i)) == 0) map.remove(S.charAt(i));
+        }
+        return res;
+    }
+
+    public int numKLenSubstrNoRepeats(String S, int K) {
+        if(S.length() < K) return 0;
+        int[] map = new int[26];
+        int res = 0, len = S.length(), end = 0, countDup = 0;
+        while(end < len) {
+            if(map[S.charAt(end) - 'a']++ > 0) {
+                countDup++;
+            }
+            if(end >= K && map[S.charAt(end-K)-'a']-- > 1) {
+                countDup--;
+            }
+            if(countDup == 0 && end >= K - 1) {
+                res++;
+            }
+            end++;
         }
         return res;
     }
@@ -60,7 +50,7 @@ public class FindKLengthSubstringsWithNoRepeatedCharacters {
         String S = "havefunonleetcode";
         int K = 5;
         FindKLengthSubstringsWithNoRepeatedCharacters f = new FindKLengthSubstringsWithNoRepeatedCharacters();
-        System.out.println(f.numKLenSubstrNoRepeats1(S, K));
+        System.out.println(f.numKLenSubstrNoRepeats(S, K));
     }
 }
 
