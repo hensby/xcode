be3daedc3ac8cc5003a3436f1a11e744efa83582
hensby <whc0721a@outlook.com>
2020-07-02 12:51:50 -0700

    7.2

--- src/matrix/PrintMatrix.java
+++ src/matrix/PrintMatrix.java
@@ -3,7 +3,7 @@ package matrix;
 import java.util.List;
 
 public class PrintMatrix {
-    public void printMatrix(int[][] matrix) {
+    public static void printMatrix(int[][] matrix) {
         for (int[] re : matrix) {
             for (int j = 0; j < re.length; j++) {
                 System.out.printf("%d ", re[j]);
@@ -12,7 +12,7 @@ public class PrintMatrix {
         }
     }
 
-    public void printMatrix(List<List<Integer>> matrix) {
+    public static void printMatrix(List<List<Integer>> matrix) {
         for (List<Integer> re : matrix) {
             for (int j = 0; j < re.size(); j++) {
                 System.out.printf("%d ", re.get(j));
