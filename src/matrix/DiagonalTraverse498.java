926871e69c337574116047a90d3aedc97d32af3e
hensby <whc0721a@outlook.com>
2020-07-02 23:29:51 -0700

    7.2

--- src/matrix/DiagonalTraverse498.java
+++ src/matrix/DiagonalTraverse498.java
@@ -3,6 +3,8 @@ package matrix;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
+
 import matrix.PrintMatrix;
 // 498
 public class DiagonalTraverse498 {
@@ -35,7 +37,7 @@ public class DiagonalTraverse498 {
             // are the respective heads.
             int r = d < M ? 0 : d - M + 1;
             int c = d < M ? d : M - 1;
-
+            System.out.println(r + " " + c);
             // Iterate until one of the indices goes out of scope
             // Take note of the index math to go down the diagonal
             while (r < N && c > -1) {
@@ -60,6 +62,40 @@ public class DiagonalTraverse498 {
         return result;
     }
 
+    int m, n;
+    public int[] findDiagonalOrder1(int[][] matrix) {
+        if(matrix == null || matrix.length == 0) return new int[0];
+        this.m = matrix[0].length;
+        this.n = matrix.length;
+        int cur = 0;
+        int[] res = new int[m * n];
+        List<Integer> middleList = new ArrayList<>();
+
+        for(int i = 0; i < m + n - 1; i++) {
+            int col = i < m? i: m - 1;
+            int row = i < m? 0: i - m + 1;
+
+            middleList.clear();
+
+            while(isValid(col, row)) {
+                middleList.add(matrix[row][col]);
+                col--;
+                row++;
+            }
+            if(i % 2 == 0) {
+                Collections.reverse(middleList);
+            }
+            for (int j = 0; j < middleList.size(); j++) {
+                res[cur++] = middleList.get(j);
+            }
+        }
+        return res;
+    }
+
+    public boolean isValid(int col ,int row) {
+        return row < n && row >= 0 && col < m && col >= 0;
+    }
+
     public static void main(String[] args) {
         int[][] matrix = new int[][]{
                 {1, 2, 3},
@@ -69,7 +105,7 @@ public class DiagonalTraverse498 {
         DiagonalTraverse498 diagonal = new DiagonalTraverse498();
 //        PrintMatrix printMatrix = new PrintMatrix();
         PrintMatrix.printMatrix(matrix);
-        System.out.println(Arrays.toString(diagonal.findDiagonalOrder(matrix)));
+        System.out.println(Arrays.toString(diagonal.findDiagonalOrder1(matrix)));
     }
 }
 
