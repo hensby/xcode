5ce630b93f6745af948133984418aa0ed75b2551
hensby <whc0721a@outlook.com>
2020-07-29 16:28:19 -0700

    7.28

---
+++ src/greedy/EraseOverlapIntervals.java
@@ -0,0 +1,31 @@
+package greedy;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class EraseOverlapIntervals {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        if (intervals.length == 0) {
+            return 0;
+        }
+        Arrays.sort(intervals, Comparator.comparingInt(o -> o[1]));
+        int cnt = 1;
+        int end = intervals[0][1];
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] < end) {
+                continue;
+            }
+            end = intervals[i][1];
+            cnt++;
+        }
+        return intervals.length - cnt;
+    }
+
+    public static void main(String[] args) {
+        int[][] input = new int[][] {
+                {1,2},{2,3},{3,4},{1,3}
+        };
+        EraseOverlapIntervals e = new EraseOverlapIntervals();
+        System.out.println(e.eraseOverlapIntervals(input));
+    }
+}
