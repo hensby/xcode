afbf6229cfae415a90f7752033fcbc2e05dfb5fb
hensby <whc0721a@outlook.com>
2020-10-11 13:26:07 -0700

    antra

--- src/antra/example/ExecutorServiceEx.java
+++ src/antra/example/ExecutorServiceEx.java
@@ -1,31 +1,54 @@
-//package antra.example;
+package antra.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
+public class ExecutorServiceEx {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+
+//        ExecutorService newSingleThreadExecutor = Executors.newSingleThreadExecutor(); // one thread
+//        newSingleThreadExecutor.execute(new Runnable() {
+//            @Override
+//            public void run() {
+//                System.out.println("int single thread");
+//            }
+//        });
+//        newSingleThreadExecutor.shutdown();
+
+        ExecutorService newMultipleThreadExecutor = Executors.newScheduledThreadPool(3);
+
+//        newMultipleThreadExecutor.execute(new Runnable() {
 //
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.concurrent.Callable;
-//import java.util.concurrent.Executor;
-//import java.util.concurrent.Executors;
-//import java.util.concurrent.ScheduledExecutorService;
-//
-//public class ExecutorServiceEx {
-//    public static void main(String[] args) {
-//
-//        ScheduledExecutorService newMultipleThreadExecutor = Executors.newScheduledThreadPool(5);
-//
-////        newMultipleThreadExecutor.execute(new Runnable() {
-////
-////            @Override
-////            public void run() {
-////                System.out.println("int one thread");
-////            }
-////        });
-//        List<Callable<String>> al = new ArrayList<>();
-//        for(int i = 1; i < 6; i++) {
-//            al.add(new Task3(i));
-//        }
-//
-//        newMultipleThreadExecutor.invokeAll(al);
-//
+//            @Override
+//            public void run() {
+//                System.out.println("int single thread");
+//            }
+//        });
 //        newMultipleThreadExecutor.shutdown();
-//    }
-//}
+
+        List<Callable<String>> al=new ArrayList();
+        for(int i = 1; i < 10; i++) {
+            al.add(new Task3(i));
+        }
+
+        List<Future<String>> invokeAll = newMultipleThreadExecutor.invokeAll(al);
+        for(Future f:invokeAll) {
+            System.out.println(f.get());
+        }
+        newMultipleThreadExecutor.shutdown();
+
+//        al.add(new Task3(1));
+//        al.add(new Task3(2));
+//        al.add(new Task3(3));
+//        al.add(new Task3(4));
+//        al.add(new Task3(5));
+//        al.add(new Task3(6));
+//        al.add(new Task3(7));
+//        al.add(new Task3(8));
+//        al.add(new Task3(9));
+//        al.add(new Task3(10));
+
+
+    }
+}
--- src/antra/example/Task3.java
+++ src/antra/example/Task3.java
@@ -1,10 +1,19 @@
 package antra.example;
 
+import java.time.LocalDateTime;
 import java.util.concurrent.Callable;
 
 public class Task3 implements Callable {
+
+    int i;
+    public Task3(int i) {
+        this.i=i;
+    }
+
     @Override
-    public Object call() throws Exception {
-        return null;
+    public String call() throws Exception {
+        LocalDateTime myDateObj = LocalDateTime.now();
+
+        return "Thread" + i + myDateObj;
     }
 }
