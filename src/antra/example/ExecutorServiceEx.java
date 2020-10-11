fb96820d1d1b009c8ef9e180735605d74d01c6d8
hensby <whc0721a@outlook.com>
2020-10-11 13:30:33 -0700

    antra

--- src/antra/example/ExecutorServiceEx.java
+++ src/antra/example/ExecutorServiceEx.java
@@ -7,25 +7,17 @@ import java.util.concurrent.*;
 public class ExecutorServiceEx {
     public static void main(String[] args) throws InterruptedException, ExecutionException {
 
-//        ExecutorService newSingleThreadExecutor = Executors.newSingleThreadExecutor(); // one thread
-//        newSingleThreadExecutor.execute(new Runnable() {
-//            @Override
-//            public void run() {
-//                System.out.println("int single thread");
-//            }
-//        });
-//        newSingleThreadExecutor.shutdown();
+        ExecutorService newSingleThreadExecutor = Executors.newSingleThreadExecutor(); // one thread
+        newSingleThreadExecutor.execute(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("int single thread");
+            }
+        });
+        newSingleThreadExecutor.shutdown();
 
         ExecutorService newMultipleThreadExecutor = Executors.newScheduledThreadPool(3);
 
-//        newMultipleThreadExecutor.execute(new Runnable() {
-//
-//            @Override
-//            public void run() {
-//                System.out.println("int single thread");
-//            }
-//        });
-//        newMultipleThreadExecutor.shutdown();
 
         List<Callable<String>> al=new ArrayList();
         for(int i = 1; i < 10; i++) {
@@ -38,17 +30,14 @@ public class ExecutorServiceEx {
         }
         newMultipleThreadExecutor.shutdown();
 
-//        al.add(new Task3(1));
-//        al.add(new Task3(2));
-//        al.add(new Task3(3));
-//        al.add(new Task3(4));
-//        al.add(new Task3(5));
-//        al.add(new Task3(6));
-//        al.add(new Task3(7));
-//        al.add(new Task3(8));
-//        al.add(new Task3(9));
-//        al.add(new Task3(10));
-
+        //        newMultipleThreadExecutor.execute(new Runnable() {
+//
+//            @Override
+//            public void run() {
+//                System.out.println("int single thread");
+//            }
+//        });
+//        newMultipleThreadExecutor.shutdown();
 
     }
 }
