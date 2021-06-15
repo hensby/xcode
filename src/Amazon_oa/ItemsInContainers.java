bdcdb3cef343a03db520328559d643d12eeff827
vn51fp2 <SXYwhc240531!>
2021-06-14 21:09:26 -0700

    1

--- src/Amazon_oa/ItemsInContainers.java
+++ src/Amazon_oa/ItemsInContainers.java
@@ -1,8 +1,6 @@
 package Amazon_oa;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class ItemsInContainers {
 
@@ -34,11 +32,67 @@ public class ItemsInContainers {
         return res;
     }
 
-        public static void main(String[] args) {
+
+    public static List<Integer> numberOfItems1(String s, List<Integer> startIndices, List<Integer> endIndices) {
+        int n = s.length();
+        Map<Integer, Integer> map = new HashMap<>();
+        int sum = 0;
+        int cumSum = 0;
+        boolean started = false;
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == '|') {
+                if (started) {
+                    cumSum += sum;
+                    map.put(i, cumSum);
+                } else
+                    map.put(i, 0);
+                started = true;
+                sum = 0;
+            } else {
+                sum++;
+            }
+        }
+        int right = -1;
+        int[] rightBoundary = new int[n];
+        for (int i = n - 1; i >= 0; i--) {
+            if (s.charAt(i) == '|') {
+                right = i;
+            }
+            rightBoundary[i] = right;
+        }
+        int left = -1;
+        int[] leftBoundary = new int[n];
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == '|') {
+                left = i;
+            }
+            leftBoundary[i] = left;
+        }
+        List<Integer> res = new ArrayList<>();
+
+        for (int i = 0; i < startIndices.size(); i++) {
+            int start = rightBoundary[startIndices.get(i) - 1];
+            int end = leftBoundary[endIndices.get(i) - 1];
+            if (start != -1 && right != -1 && start < end) {
+                res.add(map.get(end) - map.get(start));
+            } else
+                res.add(0);
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
         String s = "|**|*|*";
-        List<Integer> startIndices = new ArrayList<Integer>(){{add(1);add(1);}};
-        List<Integer> endIndices =  new ArrayList<Integer>(){{add(5);add(6);}};
-        System.out.println(numberOfItems(s, startIndices, endIndices));
+        List<Integer> startIndices = new ArrayList<Integer>() {{
+            add(1);
+            add(1);
+        }};
+        List<Integer> endIndices = new ArrayList<Integer>() {{
+            add(5);
+            add(6);
+        }};
+        System.out.println(numberOfItems1(s, startIndices, endIndices));
     }
 }
 
