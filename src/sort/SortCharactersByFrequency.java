662ce67b2235c3780d419658465ec3db48297422
vn51fp2 <SXYwhc240531!>
2021-07-11 23:16:40 -0700

    11

--- src/sort/SortCharactersByFrequency.java
+++ src/sort/SortCharactersByFrequency.java
@@ -25,14 +25,65 @@ public class SortCharactersByFrequency {
         return res.toString();
     }
 
+    public String frequencySort1(String s) {
+        Map<Character, Integer> occurMap = new HashMap<>();
+        StringBuilder sb = new StringBuilder();
+        for(char i: s.toCharArray()) {
+            occurMap.put(i, occurMap.getOrDefault(i, 0) + 1);
+        }
+        PriorityQueue<Character> queue = new PriorityQueue<>((c1, c2) -> Integer.compare(occurMap.get(c2), occurMap.get(c1)));
+        for(char i: occurMap.keySet()) {
+            if(occurMap.get(i) != 0) queue.add(i);
+        }
+        while(!queue.isEmpty()) {
+            char tmp = queue.poll();
+            int i = occurMap.get(tmp);
+            for(int j = 0; j < i; j++){
+                sb.append(tmp);
+            }
+        }
+        return sb.toString();
+    }
+
     public static void main(String[] args) {
         SortCharactersByFrequency sortCharactersByFrequency = new SortCharactersByFrequency();
         System.out.println(sortCharactersByFrequency.frequencySort("aaacccsffffff"));
-        Map<Character, Character> map = new HashMap<Character, Character>(){{
-            put('(', ')');
-            put('[', ']');
-            put('{', '}');
-        }};
+        System.out.println(sortCharactersByFrequency.frequencySort1("aaacccsffffff"));
+
+//        Map<Character, Character> map = new HashMap<Character, Character>(){{
+//            put('(', ')');
+//            put('[', ']');
+//            put('{', '}');
+//        }};
 
     }
 }
+//451. Sort Characters By Frequency
+//        Given a string s, sort it in decreasing order based on the frequency of characters, and return the sorted string.
+//
+//
+//
+//        Example 1:
+//
+//        Input: s = "tree"
+//        Output: "eert"
+//        Explanation: 'e' appears twice while 'r' and 't' both appear once.
+//        So 'e' must appear before both 'r' and 't'. Therefore "eetr" is also a valid answer.
+//        Example 2:
+//
+//        Input: s = "cccaaa"
+//        Output: "aaaccc"
+//        Explanation: Both 'c' and 'a' appear three times, so "aaaccc" is also a valid answer.
+//        Note that "cacaca" is incorrect, as the same characters must be together.
+//        Example 3:
+//
+//        Input: s = "Aabb"
+//        Output: "bbAa"
+//        Explanation: "bbaA" is also a valid answer, but "Aabb" is incorrect.
+//        Note that 'A' and 'a' are treated as two different characters.
+//
+//
+//        Constraints:
+//
+//        1 <= s.length <= 5 * 105
+//        s consists of English letters and digits.
---
+++ src/sort/SortColors.java
@@ -0,0 +1,82 @@
+package sort;
+
+import java.util.Arrays;
+
+public class SortColors {
+
+    public static void sortColors(int[] nums) {
+        int n = nums.length;
+        int p0 = 0, p2 = n - 1;
+        for (int i = 0; i <= p2; ++i) {
+            while (i <= p2 && nums[i] == 2) {
+                int temp = nums[i];
+                nums[i] = nums[p2];
+                nums[p2] = temp;
+                --p2;
+            }
+            if (nums[i] == 0) {
+                int temp = nums[i];
+                nums[i] = nums[p0];
+                nums[p0] = temp;
+                ++p0;
+            }
+        }
+    }
+
+    public static void sortColors1(int[] nums) {
+        int zero = -1, one = 0, two = nums.length;
+        while (one < two) {
+            if (nums[one] == 0) {
+                swap(nums, ++zero, one++);
+            } else if (nums[one] == 2) {
+                swap(nums, --two, one);
+            } else {
+                ++one;
+            }
+        }
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[] {2,0,2,1,1,0};
+        sortColors1(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
+//75. Sort Colors
+//        Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+//
+//        We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+//
+//        You must solve this problem without using the library's sort function.
+//
+//
+//
+//        Example 1:
+//
+//        Input: nums = [2,0,2,1,1,0]
+//        Output: [0,0,1,1,2,2]
+//        Example 2:
+//
+//        Input: nums = [2,0,1]
+//        Output: [0,1,2]
+//        Example 3:
+//
+//        Input: nums = [0]
+//        Output: [0]
+//        Example 4:
+//
+//        Input: nums = [1]
+//        Output: [1]
+//
+//
+//        Constraints:
+//
+//        n == nums.length
+//        1 <= n <= 300
+//        nums[i] is 0, 1, or 2.
