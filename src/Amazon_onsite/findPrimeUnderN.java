194af54a6ba76d8687427bf2378884c178390ae3
hensby <whc0721a@outlook.com>
2021-07-25 14:49:50 -0700

    new

---
+++ src/Amazon_onsite/findPrimeUnderN.java
@@ -0,0 +1,35 @@
+package Amazon_onsite;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class findPrimeUnderN {
+
+    public static List<Integer> findPrimeUnderN (int n) {
+        if (n < 2) return new ArrayList<>();
+        boolean[] isPrime = new boolean[n]; // false;
+        Arrays.fill(isPrime,true);
+        List<Integer> res = new ArrayList<>();
+        for (int i = 2; i*i < n; i++) {
+            if (isPrime[i]) {
+                int jjjj = i;
+                while (i * jjjj < n) { //  i = 2, j = 2
+                    if (isPrime[i * jjjj]) isPrime[i * jjjj] = false;
+                    jjjj++;
+                }
+            }
+        }
+
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i]) {
+                res.add(i);
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findPrimeUnderN(120));
+    }
+}
