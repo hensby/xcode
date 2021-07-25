9adba2a91af86b774e2b2600db6b976f9726b8c4
hensby <whc0721a@outlook.com>
2021-07-25 14:53:36 -0700

    new

--- src/Amazon_onsite/findPrimeUnderN.java
+++ src/Amazon_onsite/findPrimeUnderN.java
@@ -11,7 +11,7 @@ public class findPrimeUnderN {
         boolean[] isPrime = new boolean[n]; // false;
         Arrays.fill(isPrime,true);
         List<Integer> res = new ArrayList<>();
-        for (int i = 2; i*i < n; i++) {
+        for (int i = 2; i*i < n; i++) { // sqrt(n) log n
             if (isPrime[i]) {
                 int jjjj = i;
                 while (i * jjjj < n) { //  i = 2, j = 2
@@ -33,3 +33,5 @@ public class findPrimeUnderN {
         System.out.println(findPrimeUnderN(120));
     }
 }
+// gived positive integer n. find all primes less than n;
+// 14-> [2,3,5,7,11,13]
