4a20fcf07ff235a326541b332352c83f325727c0
hensby <whc0721a@outlook.com>
2021-07-27 23:01:03 -0700

    new

--- src/gragh/CourseScheduleII.java
+++ src/gragh/CourseScheduleII.java
@@ -3,6 +3,39 @@ package gragh;
 import java.util.*;
 
 public class CourseScheduleII { // 拓扑排序
+
+    public static int[] findOrder1(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adjacency = new ArrayList<>();
+        int[] inDegree = new int[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            adjacency.add(new ArrayList<>());
+        }
+        for (int[] pre : prerequisites) {
+            adjacency.get(pre[1]).add(pre[0]);
+            inDegree[pre[0]]++;
+        }
+        int[] res = new int[numCourses];
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i = 0; i < numCourses; i++) {
+            if(inDegree[i] == 0) queue.add(i);
+        }
+        int count = 0;
+        while (!queue.isEmpty()) {
+            int course = queue.poll();
+            res[count] = course;
+            count++;
+            for (int nextCourse: adjacency.get(course)) {
+                inDegree[nextCourse]--;
+                if (inDegree[nextCourse] == 0) {
+                    queue.add(nextCourse);
+                }
+            }
+        }
+        if (count == numCourses) return res;
+        return new int[0];
+    }
+
+
     public static int[] findOrder(int numCourses, int[][] prerequisites) {
         int[] inDegree = new int[numCourses];
         List<List<Integer>> adjacency = new ArrayList<List<Integer>>();
@@ -36,8 +69,8 @@ public class CourseScheduleII { // 拓扑排序
 
     public static void main(String[] args) {
         int numCourses = 4;
-        int[][] prerequisites = new int[][] {{1,0},{2,0},{3,1},{3,2}};
-        System.out.println(Arrays.toString(findOrder(numCourses, prerequisites)));
+        int[][] prerequisites = new int[][]{{1, 0}, {2, 0}, {3, 1}, {3, 2}};
+        System.out.println(Arrays.toString(findOrder1(numCourses, prerequisites)));
     }
 }
 //210. Course Schedule II
