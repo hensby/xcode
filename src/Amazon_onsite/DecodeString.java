b2ccb20d73e2d42d24a90e486bcdc0cafb670da7
hensby <whc0721a@outlook.com>
2021-08-04 17:05:23 -0700

    new

--- src/Amazon_onsite/DecodeString.java
+++ src/Amazon_onsite/DecodeString.java
@@ -1,7 +1,37 @@
 package Amazon_onsite;
 
+import java.util.LinkedList;
+
 public class DecodeString {
 
+    public static String decodeString(String s) {
+        StringBuilder res = new StringBuilder();
+        int multi = 0;
+        LinkedList<Integer> stack_multi = new LinkedList<>();
+        LinkedList<String> stack_res = new LinkedList<>();
+        for (Character c : s.toCharArray()) {
+            System.out.println(stack_multi);
+            System.out.println(stack_res);
+            if (c == '[') {
+                stack_multi.addLast(multi);
+                stack_res.addLast(res.toString());
+                multi = 0;
+                res = new StringBuilder();
+            } else if (c == ']') {
+                StringBuilder tmp = new StringBuilder();
+                int cur_multi = stack_multi.removeLast();
+                for (int i = 0; i < cur_multi; i++) tmp.append(res);
+                res = new StringBuilder(stack_res.removeLast() + tmp);
+            } else if (c >= '0' && c <= '9') multi = multi * 10 + Integer.parseInt(c + "");
+            else res.append(c);
+        }
+
+        return res.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(decodeString("3[a2[c]]"));
+    }
 
 }
 //394. Decode String
