395dfe417c6ba3bda407190e2a335731522c3001
hensby <whc0721a@outlook.com>
2021-08-05 10:37:41 -0700

    new

---
+++ src/Amazon_onsite/FindKLengthSubstringsWithNoRepeatedCharacters.java
@@ -0,0 +1,76 @@
+package Amazon_onsite;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FindKLengthSubstringsWithNoRepeatedCharacters {
+
+
+    public int numKLenSubstrNoRepeats1(String S, int K) {
+        if (S.length() < K) return 0;
+        int res = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        int i = 0;
+        int j = 0;
+        for (; j < K - 1; j++) {
+            map.put(S.charAt(j), map.getOrDefault(S.charAt(j), 0) + 1);
+        }
+        for (; j < S.length(); j++,i++) {
+            System.out.println(map);
+            map.put(S.charAt(j), map.getOrDefault(S.charAt(j), 0) + 1);
+            if (map.size() == K) res++;
+            map.put(S.charAt(i), map.getOrDefault(S.charAt(i), 0) - 1);
+            if (map.get(S.charAt(i)) == 0) map.remove(S.charAt(i));
+        }
+        return res;
+    }
+
+    public int numKLenSubstrNoRepeats(String S, int K) {
+        if(S.length() < K) return 0;
+        int[] map = new int[26];
+        int res = 0, len = S.length(), end = 0, countDup = 0;
+        while(end < len) {
+            if(map[S.charAt(end) - 'a']++ > 0) {
+                countDup++;
+            }
+            if(end >= K && map[S.charAt(end-K)-'a']-- > 1) {
+                countDup--;
+            }
+            if(countDup == 0 && end >= K - 1) {
+                res++;
+            }
+            end++;
+        }
+        return res;
+    }
+
+
+    public static void main(String[] args) {
+        String S = "havefunonleetcode";
+        int K = 5;
+        FindKLengthSubstringsWithNoRepeatedCharacters f = new FindKLengthSubstringsWithNoRepeatedCharacters();
+        System.out.println(f.numKLenSubstrNoRepeats1(S, K));
+    }
+}
+
+//          LeetCode 1100. Find K-Length Substrings With No Repeated Characters
+//
+//        Given a string S, return the number of substrings of length K with no repeated characters.
+//
+//        Example 1:
+//
+//        Input: S = "havefunonleetcode", K = 5
+//        Output: 6
+//        Explanation:
+//        There are 6 substrings they are : 'havef','avefu','vefun','efuno','etcod','tcode'.
+//        Example 2:
+//
+//        Input: S = "home", K = 5
+//        Output: 0
+//        Explanation:
+//        Notice K can be larger than the length of S. In this case is not possible to find any substring.
+//        Note:
+//
+//        1 <= S.length <= 10^4
+//        All characters of S are lowercase English letters.
+//        1 <= K <= 10^4
---
+++ src/Amazon_onsite/Subsets.java
@@ -0,0 +1,69 @@
+package Amazon_onsite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subsets {
+
+    List<List<Integer>> res = new ArrayList<>();
+    int[] nums;
+    int len;
+
+    public List<List<Integer>> subsets(int[] nums) {
+        if (nums == null || nums.length == 0) return res;
+        this.nums = nums;
+        this.len = nums.length;
+        boolean[] isVisit = new boolean[len];
+        List<Integer> tmpRes = new ArrayList<>();
+        for (int i = 0; i <= len; i++) {
+            dfs(tmpRes, isVisit, 0, i);
+        }
+
+        return res;
+    }
+
+    public void dfs(List<Integer> tmpRes, boolean[] isVisit, int start, int size) {
+        if (tmpRes.size() == size) {
+            res.add(new ArrayList<>(tmpRes));
+            return;
+        }
+
+        for (int i = start; i < len; i++) {
+            if (!isVisit[i]) {
+                isVisit[i] = true;
+                tmpRes.add(nums[i]);
+                dfs(tmpRes, isVisit, i + 1, size);
+                isVisit[i] = false;
+                tmpRes.remove(tmpRes.size() - 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[] {1,2,3};
+        Subsets s = new Subsets();
+        System.out.println(s.subsets(nums));
+    }
+}
+//78. Subsets
+//        Given an integer array nums of unique elements, return all possible subsets (the power set).
+//
+//        The solution set must not contain duplicate subsets. Return the solution in any order.
+//
+//
+//
+//        Example 1:
+//
+//        Input: nums = [1,2,3]
+//        Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+//        Example 2:
+//
+//        Input: nums = [0]
+//        Output: [[],[0]]
+//
+//
+//        Constraints:
+//
+//        1 <= nums.length <= 10
+//        -10 <= nums[i] <= 10
+//        All the numbers of nums are unique.
