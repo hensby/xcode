a0a73e0aa36f5a90bb26764f3dfe4cc088aebfe2
Administrator <whc0721a@vip.qq.com>
2022-08-24 21:56:24 -0700

    new

--- src/OOD/MyHashMap.java
+++ src/OOD/MyHashMap.java
@@ -1,74 +1,91 @@
-import java.util.ArrayList;
-import java.util.List;
+package OOD;
 
 public class MyHashMap {
-    public node[] hashValue;
-    /** Initialize your data structure here. */
+
+    private Node[] hashMapBucket;
+
     public MyHashMap() {
-        int numberBucket = 2067;
-        hashValue = new node[numberBucket];
+        hashMapBucket = new Node[500];
     }
 
-    /** value will always be non-negative. */
     public void put(int key, int value) {
-        int hash = hashFunction(key);
-        System.out.println(hashValue[hash]);
-        node tmp = new node(key, value);
-        if (hashValue[hash] == null) {
-            hashValue[hash] = tmp;
-        }
-        else {
-            node cur = hashValue[hash];
-            cur.next = tmp;
-        }
-    }
-
-    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
-    public int get(int key) {
-        int hash = hashFunction(key);
-        if (hashValue[hash] == null) return -1;
-        else {
-            node cur = hashValue[hash];
-            while(cur!= null) {
-                if(cur.key == key) return cur.value;
-                else cur = cur.next;
-            }
-            return -1;
-        }
-    }
-
-    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
-    public void remove(int key) {
-        if (get(key) == -1) return;
-        else {
-            int hash = hashFunction(key);
-            node cur = hashValue[hash];
-
-            while(cur.next != null) {
-                node prev = cur; cur = cur.next;
-                if(cur.key == key) {
-                    prev.next = cur.next;
-                    cur = cur.next;
-                }
+        int hashCode = key % 500;
+        Node node = getNode(key);
+        if (node != null) {
+            node.value = value;
+        } else {
+            Node head = hashMapBucket[hashCode];
+            Node newNode = new Node(key, value);
+            if (head == null) hashMapBucket[hashCode] = newNode;
+            else {
+                hashMapBucket[hashCode] = newNode;
+                newNode.next = head;
             }
         }
         return;
     }
 
-    private int hashFunction(int key) {
-        return key % 2067;
+    public int get(int key) {
+        int hashCode = key % 500;
+        Node head = hashMapBucket[hashCode];
+        if (head == null) return -1;
+        while (head != null) {
+            if (head.key == key) {
+                return head.value;
+            } else {
+                head = head.next;
+            }
+        }
+        return -1;
     }
 
-    class node{
-        int key, value;
-        node prev, next;
+    public Node getNode(int key) {
+        int hashCode = key % 500;
+        Node head = hashMapBucket[hashCode];
+        if (head == null) return null;
+        while (head != null) {
+            if (head.key == key) {
+                return head;
+            } else {
+                head = head.next;
+            }
+        }
+        return null;
+    }
 
-        node (int key, int value) {
-            this.key = key;
-            this.value = value;
+    public void remove(int key) {
+        int hashCode = key % 500;
+        Node head = hashMapBucket[hashCode];
+        if (head == null) return;
+        Node prev = null;
+        while (head != null) {
+            if (head.key == key) {
+                if (prev == null) {
+                    hashMapBucket[hashCode] = head.next;
+                } else {
+                    Node next = head.next;
+                    prev.next = next;
+                }
+                break;
+            } else {
+                prev = head;
+                head = head.next;
+            }
         }
     }
-//class MyHashMap {
+
+    public class Node {
+        Integer value;
+        Integer key;
+        Node next;
+
+        public Node(int key, int value) {
+            this.value = value;
+            this.key = key;
+        }
+    }
+
+//class OOD.MyHashMap {
 //
 //    class Node{
 //        int key, value;
@@ -84,7 +101,7 @@ public class MyHashMap {
 //    private Node[] data = new Node[length];
 //
 //    /** Initialize your data structure here. */
-//    public MyHashMap() {
+//    public OOD.MyHashMap() {
 //
 //    }
 //
@@ -157,11 +174,15 @@ public class MyHashMap {
 
     public static void main(String[] args) {
         MyHashMap obj = new MyHashMap();
-        obj.put(1,100);
-        System.out.println(obj.hashValue[1]);
+        obj.put(1, 100);
+        obj.put(2, 1200);
+
+        System.out.println(obj.get(1));
         int param_2 = obj.get(1);
         System.out.println(param_2);
+        System.out.println(obj.get(2));
         obj.remove(1);
+        System.out.println(obj.get(1));
     }
 
 }
